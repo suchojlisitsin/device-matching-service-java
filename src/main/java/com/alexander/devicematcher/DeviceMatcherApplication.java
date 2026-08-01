8b3dda05a89801dbc20ef68fdb5f556cd8cedf8c
Alexander Kostov <alkostov@tu-sofia.bg>
2026-08-01 15:53:13 +0300

    Init

---
+++ src/main/java/com/alexander/devicematcher/DeviceMatcherApplication.java
@@ -0,0 +1,13 @@
+package com.alexander.devicematcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceMatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeviceMatcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alexander/devicematcher/DeviceMatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexander.devicematcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceMatcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
